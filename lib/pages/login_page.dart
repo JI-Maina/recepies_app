119cfe88577a1990d0c642cf6e308b55476a66e2
McMaina <irutujmaina@gmail.com>
2024-05-21 15:29:36 +0300

    created login page ui

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Login'),
+      ),
+      body: SafeArea(
+        child: _buildUI(),
+      ),
+    );
+  }
+
+  Widget _buildUI() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _title(),
+          _loginForm(),
+        ],
+      ),
+    );
+  }
+
+  Widget _title() {
+    return const Text(
+      'Recipe Book',
+      style: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+
+  Widget _loginForm() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.90,
+      height: MediaQuery.sizeOf(context).height * 0.30,
+      child: Form(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Username',
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Password',
+              ),
+            ),
+            _loginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.60,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('login'),
+      ),
+    );
+  }
+}
